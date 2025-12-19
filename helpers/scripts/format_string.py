961df50740c61bcb9425cf5c4263f4dc78b2824a
Vadim Rudakov <lefthand67@yandex.ru>
2025-12-20 01:20:27 +0500

    feat(helpers/scripts): add format_string.py script
    Add a new Python script "format_string.py" in the "helpers/scripts" directory.
    This script formats an input string by removing colons, double quotes, replacing
    spaces with underscores, and converting the entire string to lowercase.
    
    The script is designed to be run from the Linux terminal, allowing users to pass
    any string as an argument and receive the formatted output directly in
    the terminal. This can be particularly useful for standardizing text formats,
    such as creating file names or identifiers that follow specific naming
    conventions.
    
    Instructions on how to use the script are provided in a new README file
    "README_format_string.md" within the same directory.

---
+++ helpers/scripts/format_string.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 format_string.py 'Your Input String'")
+    else:
+        input_str = sys.argv[1]
+        formatted_str = format_string(input_str)
+        print(formatted_str)
+
+
+def format_string(input_string):
+    # Remove colons, double quotes, and replace spaces with underscores
+    formatted_string = (
+        input_string.replace(":", "").replace('"', "").replace(" ", "_").lower()
+    )
+    return formatted_string
+
+
+if __name__ == "__main__":
+    main()
